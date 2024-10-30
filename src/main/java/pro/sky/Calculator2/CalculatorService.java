d85bfec37c52e05e39fe8d5803f09478033497be
Pavel-Kuznetsov-111 <gilrok@rambler.ru>
2024-10-30 23:50:47 +0300

    Домашнее задание по теме: Вводное в Maven и Spring Boot + web

---
+++ src/main/java/pro/sky/Calculator2/CalculatorService.java
@@ -0,0 +1,22 @@
+package pro.sky.Calculator2;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public Integer plus(Integer num1, Integer num2) {
+        return num1 + num2;
+    }
+
+    public Integer minus(Integer num1, Integer num2) {
+        return num1 - num2;
+    }
+
+    public Double divide(Integer num1, Integer num2) {
+        return (double) (num1 / num2);
+    }
+
+    public Integer multiply(Integer num1, Integer num2) {
+        return num1 * num2;
+    }
+}
